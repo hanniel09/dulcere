bec56cd166d2b0c7e6ad80ae1d2fa170caadad96
hanniel09 <hannielvieira1227@gmail.com>
2024-08-28 23:57:42 -0300

    feat

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -5,12 +5,14 @@
 import * as $$$$$$$$$$$0 from "./apps/deco/htmx.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/Footer.tsx";
+import * as $$$$$$3 from "./sections/header.tsx";
 import * as $$$$$$1 from "./sections/ItWorks.tsx";
 import * as $$$$$$2 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
     "site/sections/Footer.tsx": $$$$$$0,
+    "site/sections/header.tsx": $$$$$$3,
     "site/sections/ItWorks.tsx": $$$$$$1,
     "site/sections/Theme/Theme.tsx": $$$$$$2,
   },
---
+++ sections/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div class="bg-rose-400 bg-opacity-50">
+      <h1 class="text-3xl">Dulcere</h1>
+    </div>
+  );
+}
